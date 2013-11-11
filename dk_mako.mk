3e39cfc15f8e6320ea0c714df37d00b3735b00e1
XXMrHyde <xxmrhyde@gmx.co.uk>
2013-11-11 21:42:30 +0100

    Add dk_mako build target

--- dk_mako.mk
+++ dk_mako.mk
@@ -30,12 +30,14 @@ PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
-PRODUCT_NAME := full_mako
+# Inherit from the darkkat vendor common configuration
+$(call inherit-product, vendor/dk/config/common.mk)
+
+PRODUCT_NAME := dk_mako
 PRODUCT_DEVICE := mako
-PRODUCT_BRAND := Android
+PRODUCT_BRAND := Google
 PRODUCT_MODEL := Nexus 4
 PRODUCT_MANUFACTURER := LGE
-PRODUCT_RESTRICT_VENDOR_FILES := true
 
 # Inherit from hardware-specific part of the product configuration
 $(call inherit-product, device/lge/mako/device.mk)
